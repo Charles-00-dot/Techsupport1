dce8d687a153e1a1ddf2426d49449ba73b98f53f
Charles David Endaya <charlzjavier0122@gmail.com>
2025-03-09 13:44:23 -0400

    New version

--- MapTester.java
+++ MapTester.java
@@ -10,16 +10,23 @@ import java.util.HashMap;
  * 31. it returns null.
  * 32.keySet();
  * 33.
- * 34.
- * 35.
- * 36.
- * 37.
- * 38.
+ * 34.Both HashSet and ArrayList are part of the Java Collections Framework,
+ * but they implement different interfaces: HashSet implements Set, and ArrayList implements List.
+ * 35.The split() method in Java allows you to define exactly how a string 
+ * should be split using regular expressions (regex). You can specify delimiters, patterns, or conditions that determine where the string should be split.
+ * String str = "Hello World";
+   String[] parts = str.split(" ");  // Split by space
+   System.out.println(Arrays.toString(parts));  // Output: [Hello, World]
+
  * 
- * 
- * 
- * 
- * 
+ * 36.You can use the regex \\s+ to split by any whitespace (space, tab, etc.).
+ * 37.The words in a HashSet are not guaranteed to be in any specific order. 
+ * The HashSet does not maintain the order in which elements are added.
+ * The words in an ArrayList are returned in the order they were added. It maintains the insertion order.
+ * 38.When using the split() method in Java, if there are multiple spaces 
+ * (e.g., two or three spaces) between words, it depends on how you define the 
+ * delimiter in the regular expression. If you don't account for multiple spaces, you could end up with empty strings in your result.
+ * 39.
  * 
  */
 public class MapTester
